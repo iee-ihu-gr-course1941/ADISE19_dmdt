6f3e88858af38991c32a515bca50b8c2dd7f198a
Konstantinos Ts <Konstantinos.ts@users.noreply.github.com>
2019-12-22 15:02:27 +0200

    edited

--- lib/board.php
+++ lib/board.php
@@ -34,5 +34,62 @@ function new_board() {
 	show_board();
 }
 
+function play_pos(){
+	global $mysqli;
+	
+	$X=$_POST["posX"];
+	$Y=$_POST["posY"];
+	$sql="INSERT INTO moves(game,player,posX,posY) VALUES (".$_SESSION['gameID'].",".$_SESSION['name'].",$X,$Y)";
+	
+	$mysqli->query($sql);
+}
+
+function start_game()
+{
+	global $mysqli;
+	
+	$_SESSION["name"]=$_POST["name"];
+	$name=$_SESSION["name"];
+	
+	$result = mysqli_query( $mysqli , "SELECT ID FROM game WHERE status='initialized'" );
+	$line = mysqli_fetch_row($result);
+	$info = array();
+
+	if(!isset($line[0]))
+	{
+		$sql="INSERT INTO game( playerx , status) VALUES ( '$name ', 'initialized')";
+		$mysqli->query($sql);
+		$id = $mysqli->insert_id;
+		$info["gameID"]=$id;
+		$info["XO"]="X";
+		echo json_encode($info);
+	}
+	else
+	{
+		$sql="UPDATE game SET status='started', playery='$name' where id=$line[0]";
+		$id=$line[0];
+		$mysqli->query($sql);
+		$info["gameID"]=$id;
+		$info["XO"]="O";
+		echo json_encode($info);
+
+	
+	}
+}
+
+function get_turn()
+{
+	global $mysqli;
+	$result = mysqli_query( $mysqli , "SELECT turn FROM game WHERE id=".$_SESSION['gameID'] );
+	$line = mysqli_fetch_row($result);
+	echo json_encode($line[0]);
+}
+
+function endGame()
+{
+	$sql="UPDATE game SET status='ended' where id=".$_SESSION["gameID"];
+	$mysqli->query($sql);
+	unset($_SESSION["gameID"]);
+}
 
 ?>
