6a707092721086e02653f0173fab2ef3ccdf3bf6
Jimmy M <57236583+jimmy10969@users.noreply.github.com>
2019-12-31 14:16:37 +0200

    show result

--- js/mytriliza.js
+++ js/mytriliza.js
@@ -117,7 +117,7 @@ function update_status(data) {
 }
 
 function update_info(){
-	$('#game_info').html("I am Player: "+me.piece+", my name is "+me.username +'<br>Token='+me.token+'<br>Game state: '+game_status.status+', '+ game_status.p_turn+' must play now.');
+	$('#game_info').html("I am Player: "+me.piece+", my name is "+me.username +'<br>Token='+me.token+'<br>Game state: '+game_status.status+', '+ game_status.p_turn+' must play now , the result is ' + game_status.result);
 	
 }
 
